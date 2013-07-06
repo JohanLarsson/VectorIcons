3963cc186b55b42c91f04f247cb67f68401e0821
JohanLarsson <johan_gavle1@hotmail.com>
2013-07-06 11:44:09 +0200

    Refactored to style

---
+++ VectorIcons/Converters/MultiplyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace VectorIcons.Converters
+{
+    public class MultiplyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = System.Convert.ToDouble(value);
+            double p = System.Convert.ToDouble(parameter);
+            return d * p;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

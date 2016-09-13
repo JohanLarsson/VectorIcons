8a3a067be2ba0dc50120a0ff0ad5b48d3402f33f
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-14 01:53:17 +0200

    wip

--- DigitTrimmer/TextConverter.cs
+++ DigitTrimmer/TextConverter.cs
@@ -13,8 +13,8 @@
 
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var s = (string)values[0];
-            if (string.IsNullOrEmpty(s))
+            var text = (string)values[0];
+            if (string.IsNullOrEmpty(text))
             {
                 return string.Empty;
             }
@@ -22,26 +22,35 @@
             var digits = string.IsNullOrEmpty((string)values[1])
                              ? 0
                              : int.Parse((string)values[1], CultureInfo.InvariantCulture);
-            var format = digits == 0
-                                ? "0"
-                                : $"0.{new string('#', digits)}";
-
             var scale = string.IsNullOrEmpty((string)values[2])
-                               ? 1
-                               : double.Parse((string)values[2], CultureInfo.InvariantCulture);
-
-            var replace = DoubleRegex.Replace(s, m =>
-                {
-                    var d = double.Parse(m.Value, CultureInfo.InvariantCulture);
-                    d *= scale;
-                    return d.ToString(format, CultureInfo.InvariantCulture);
-                });
-            return replace;
+                   ? 1
+                   : double.Parse((string)values[2], CultureInfo.InvariantCulture);
+            return Convert(text, digits, scale);
         }
 
         object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        public static string Convert(string text, int digits, double scale)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            var format = digits == 0
+                                ? "0"
+                                : $"0.{new string('#', digits)}";
+
+            var replace = DoubleRegex.Replace(text, m =>
+            {
+                var d = double.Parse(m.Value, CultureInfo.InvariantCulture);
+                d *= scale;
+                return d.ToString(format, CultureInfo.InvariantCulture);
+            });
+            return replace;
         }
     }
 }
---
+++ DigitTrimmer/ViewModel.cs
@@ -0,0 +1,119 @@
+﻿namespace DigitTrimmer
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Media;
+    using JetBrains.Annotations;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string input;
+        private string output;
+        private int digits;
+        private bool shiftToOrigin;
+        private double size;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Input
+        {
+            get { return this.input; }
+
+            set
+            {
+                if (value == this.input)
+                {
+                    return;
+                }
+
+                this.input = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Output));
+            }
+        }
+
+        public string Output => GetOutput();
+
+        public int Digits
+        {
+            get { return this.digits; }
+
+            set
+            {
+                if (value == this.digits)
+                {
+                    return;
+                }
+
+                this.digits = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Output));
+            }
+        }
+
+        public bool ShiftToOrigin
+        {
+            get { return this.shiftToOrigin; }
+
+            set
+            {
+                if (value == this.shiftToOrigin)
+                {
+                    return;
+                }
+
+                this.shiftToOrigin = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Output));
+            }
+        }
+
+        public double Size
+        {
+            get { return this.size; }
+
+            set
+            {
+                if (value.Equals(this.size))
+                {
+                    return;
+                }
+
+                this.size = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Output));
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string GetOutput()
+        {
+            if (string.IsNullOrWhiteSpace(this.input))
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                return TextConverter.Convert(this.input, this.digits, 1);
+                var tokens = Parser.GetTokens(this.input).ToArray();
+
+                return "";
+                //var result = geometry.ToString(CultureInfo.InvariantCulture);
+                //return result;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
